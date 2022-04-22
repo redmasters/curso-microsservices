d7b6005884e63a2d50e3817724033aa1827a5eeb
Erasmo Jr <erasmo.junior@gmail.com>
2022-04-22 14:47:05 -0300

    Open Feign Implemented

--- hr-payroll/src/main/java/io/red/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/io/red/hrpayroll/services/PaymentService.java
@@ -1,29 +1,22 @@
 package io.red.hrpayroll.services;
 
-import io.red.hrpayroll.configuration.RestTemplateConfig;
 import io.red.hrpayroll.entities.Payment;
 import io.red.hrpayroll.entities.Worker;
-import org.springframework.beans.factory.annotation.Value;
+import io.red.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-    private final RestTemplateConfig restTemplateConfig;
+    private final WorkerFeignClient workerFeignClient;
 
-    public PaymentService(RestTemplateConfig restTemplateConfig) {
-        this.restTemplateConfig = restTemplateConfig;
+    public PaymentService(WorkerFeignClient workerFeignClient) {
+        this.workerFeignClient = workerFeignClient;
     }
 
+
     public Payment getPayment(long workerId, int days) {
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", "" + workerId);
-        Worker worker = restTemplateConfig.restTemplate().getForObject(workerHost + "/v1/workers/{id}", Worker.class, uriVariables);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
